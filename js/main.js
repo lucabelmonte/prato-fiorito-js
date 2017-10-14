82aa4ae94bc5b54bd3ce6ae8ea85fbd15fdfe504
Luca <x3lit@outlook.com>
2017-10-14 17:03:41 +0200

    pf-01

--- js/main.js
+++ js/main.js
@@ -1,67 +1,146 @@
-tableId = "game";
-swag = "swag";
-
-bomb = "*";
-clear = " ";
-number = ".";
-
-row = "<tr>";
-rowEnd = "</tr>";
-col = "<td>";
-colEnd = "</td>";
-content = '<input type="button" value="{x_x_x}" onclick="clickButton({y_y_y})">';
-
-var Game = function(n){
+var Game = function(nX, nY){
   /*
   * Inizializzazione delle proprietà
   */
-  this.table = tableId;
-  this.numberN = n;
 
-  this.playGround = new Array(this.numberN);
-  this.playGroundVisual = new Array(this.numberN);
+  /// constant
+  this.constN = 9;
+  this.bombPerc = .15;
 
+  this.tableButtonPLay = '<input type="button" value="{x_x_x}" style="{z_z_z}" onclick="clickButton({y_y_y})">';
+  this.HTMLelement = {
+    row: "<tr>",
+    rowEnd: "</tr>",
+    col: "<td>",
+    colEnd: "</td>",
+  };
+
+  /// game element
+
+  this.willBeNumber = ".";
+
+  this.elements = {
+    bomb: 'B',
+    clear: ' ',
+    space: '#',
+    number: '.'
+  };
+
+  this.dim = {
+    x: nX,
+    y: nY
+  };
+
+
+  this.endGame = false;
+  //this.numberN = n;
+
+  this.tableId = "game";
+
+
+  this.getX = function(){return this.dim.x;}
+  this.getY = function(){return this.dim.y;}
+
+  this.getEndGame = function(){
+    return this.endGame;
+  }
+
+  this.setEndGame = function(){
+    this.endGame = true;
+    for(var i = 0; i < this.getY(); i++){
+      for(var j = 0; j < this.getX(); j++){
+        this.playGroundVisual[i][j] = this.playGround[i][j] != this.elements.clear ? this.playGround[i][j] : this.elements.space ;
+      }
+    }
+  }
   /*
-  * Inizio costruttore
+  * Metodi interni
   */
-  for(var i = 0; i < this.numberN; i++) this.playGround[i] = new Array(this.numberN);
-  for(var i = 0; i < this.numberN; i++) this.playGroundVisual[i] = new Array(this.numberN);
+
+  this.ini = function(){
+    // creazione delle matrici
+    this.playGround = new Array(this.getY());
+    this.playGroundVisual = new Array(this.getY());
+    for(var i = 0; i < this.getY(); i++) this.playGround[i] = new Array(this.getX());
+    for(var i = 0; i < this.getY(); i++) this.playGroundVisual[i] = new Array(this.getX());
+    //this.playGroundVisual = this.playGround.slice();
 
 
-  for(var i = 0; i < this.numberN; i++) for(var j = 0; j < this.numberN; j++) this.playGround[i][j] = (Math.random() < .9) ? '.' : bomb;
-  for(var i = 0; i < this.numberN; i++) for(var j = 0; j < this.numberN; j++) this.playGroundVisual[i][j] = clear;
+    // inizializzazione delle matrici
+    for(var i = 0; i < this.getY(); i++) for(var j = 0; j < this.getX(); j++) this.playGround[i][j] = (Math.random() < (1 - this.bombPerc)) ? this.elements.number : this.elements.bomb;
+    for(var i = 0; i < this.getY(); i++) for(var j = 0; j < this.getX(); j++) this.playGroundVisual[i][j] = this.elements.clear;
 
-  for(var i = 0; i < this.numberN ; i++){
-    for(var j = 0; j < this.numberN; j++){
-      this.playGround[i][j] = this.playGround[i][j] == bomb ? bomb : this.numBombsAround(j, i);
+
+
+
+    for(var i = 0; i < this.getY(); i++){
+      for(var j = 0; j < this.getX(); j++){
+        this.playGround[i][j] = this.playGround[i][j] == this.elements.bomb ? this.elements.bomb : this.numBombsAround(j, i);
+      }
     }
   }
 
 
 
-  /*
-  * Metodi interni
-  */
   this.getPlayGround = function(){
     return this.playGround;
   }
 
   this.setPlayGroundVisual = function(val, i, j){
-    this.playGroundVisual[i][j] = val ;
+    this.playGroundVisual[i][j] = val;
   }
 
   this.createTable = function(){
     var tableString = "";
-    for(var i = 0; i < this.numberN; i++){
-      tableString += row;
-       var actualContent = content;
-      for(var j = 0; j < this.numberN; j++){
-        tableString += col + content.replace("{x_x_x}", this.playGroundVisual[i][j]).replace("{y_y_y}",  i + "," + j ) + colEnd;
+    for(var i = 0; i < this.getY(); i++){
+      tableString += this.HTMLelement.row;
+       var actualContent = this.tableButtonPLay;
+      for(var j = 0; j < this.getX(); j++){
+        tableString += this.HTMLelement.col + actualContent.replace("{x_x_x}", this.elements.clear).replace("{y_y_y}",  i + "," + j ).replace("{z_z_z}", '') + this.HTMLelement.colEnd;
       }
-      tableString += row;
+      tableString += this.HTMLelement.rowEnd;
     }
-    document.getElementById(tableId).innerHTML = tableString;
+    document.getElementById(this.tableId).innerHTML = tableString;
   };
+
+  this.updateTable = function(){
+    var tableString = "";
+    for(var i = 0; i < this.getY(); i++){
+      tableString += this.HTMLelement.row;
+       var actualContent = this.tableButtonPLay;
+      for(var j = 0; j < this.getX(); j++){
+        var element = this.playGroundVisual[i][j];
+        var elementToAdd = 'color:';
+
+        //if(element == this.element.space)
+
+        if(element == this.elements.bomb) elementToAdd += '#ff0000';
+        else if(element == this.elements.space) elementToAdd = 'background: black';
+        else if(element != NaN){
+          switch (element) {
+            case 1:
+              elementToAdd += '#3a73a9';
+              break;
+            case 2:
+              elementToAdd += '#9966ff';
+              break;
+            case 3:
+              elementToAdd += '#ff6699';
+              break;
+            default:
+              elementToAdd += '#ff0000';
+          }
+        }
+        else elementToAdd += '#ffffff';
+
+
+        tableString += this.HTMLelement.col + actualContent.replace("{x_x_x}", element).replace("{y_y_y}",  i + "," + j ).replace("{z_z_z}", elementToAdd) + this.HTMLelement.colEnd;
+      }
+      tableString += this.HTMLelement.rowEnd;
+    }
+    document.getElementById(this.tableId).innerHTML = tableString;
+  };
+
 };
 
 
@@ -69,35 +148,39 @@ Game.prototype.numBombsAround = function (x, y) {
   var cont = 0;
   var numBombsNear = new Array();
 
-  if((x > 0 && x < this.numberN - 1) && (y > 0 && y < this.numberN - 1)){
+  if((x > 0 && x < this.getX()) && (y > 0 && y < this.getY() - 1)){
     numBombsNear.push(this.playGround[y][x + 1], this.playGround[y][x - 1], this.playGround[y + 1][x + 1], this.playGround[y + 1][x - 1], this.playGround[y - 1][x + 1], this.playGround[y - 1][x - 1], this.playGround[y + 1][x], this.playGround[y - 1][x]);
   } else if (x == 0) {
     if(y == 0) numBombsNear.push(this.playGround[y][x + 1], this.playGround[y + 1][x + 1], this.playGround[y + 1][x]);
-    else if (y == this.numberN - 1) numBombsNear.push(this.playGround[y][x + 1],  this.playGround[y - 1][x], this.playGround[y - 1][x + 1]);
+    else if (y == this.getY() - 1) numBombsNear.push(this.playGround[y][x + 1],  this.playGround[y - 1][x], this.playGround[y - 1][x + 1]);
     else numBombsNear.push(this.playGround[y][x + 1], this.playGround[y + 1][x + 1], this.playGround[y + 1][x], this.playGround[y - 1][x], this.playGround[y - 1][x + 1]);
-  } else if(x == this.numberN - 1){
+  } else if(x == this.getX() - 1){
     if(y == 0) numBombsNear.push(this.playGround[y][x - 1], this.playGround[y + 1][x - 1], this.playGround[y + 1][x]);
-    else if (y == this.numberN - 1) numBombsNear.push(this.playGround[y][x - 1],  this.playGround[y - 1][x], this.playGround[y - 1][x - 1]);
+    else if (y == this.getY() - 1) numBombsNear.push(this.playGround[y][x - 1],  this.playGround[y - 1][x], this.playGround[y - 1][x - 1]);
     else numBombsNear.push(this.playGround[y][x - 1], this.playGround[y + 1][x - 1], this.playGround[y + 1][x], this.playGround[y - 1][x], this.playGround[y - 1][x - 1]);
   } else if (y == 0) {
     numBombsNear.push(this.playGround[y][x + 1], this.playGround[y][x - 1], this.playGround[y + 1][x + 1], this.playGround[y + 1][x - 1], this.playGround[y + 1][x]);
-  } else if (y == this.numberN - 1) {
+  } else if (y == this.getY() - 1) {
     numBombsNear.push(this.playGround[y][x + 1], this.playGround[y][x - 1], this.playGround[y - 1][x + 1], this.playGround[y - 1][x - 1], this.playGround[y - 1][x]);
   }
 
-  for (var i in numBombsNear) if(numBombsNear[i] == bomb) cont++;
-  return ((cont == 0) ? " " : cont) ;
-  var x;
+  for (var i in numBombsNear) if(numBombsNear[i] == this.elements.bomb) cont++;
+  return ((cont == 0) ? this.elements.clear : cont) ;
 };
 
 Game.prototype.blankArea = function (i, j) {
   //console.log("i: " + i + " j: " + j);
-  if(i < 0 || i > this.numberN - 1 || j < 0 || j > this.numberN - 1) return;
-  else if(this.playGround[i][j] == bomb || this.playGround[i][j] != clear) return;
+  if(i < 0 || i > this.getY() - 1 || j < 0 || j > this.getX() - 1) return;
+  else if(this.playGround[i][j] == this.elements.bomb) return;
+  else if(this.playGround[i][j] != this.elements.clear) {
+    this.playGroundVisual[i][j] = this.playGround[i][j];
+    return;
+    return;
+  }
   else {
-    this.playGround[i][j] = "#";
-    this.playGroundVisual[i][j] = "#";
-    this.createTable();
+    this.playGround[i][j] = this.elements.space;
+    this.playGroundVisual[i][j] = this.elements.space;
+
     this.blankArea(i + 1, j);
     this.blankArea(i - 1, j);
     this.blankArea(i, j + 1);
@@ -108,14 +191,19 @@ Game.prototype.blankArea = function (i, j) {
     this.blankArea(i - 1, j + 1);
     this.blankArea(i - 1, j - 1);
   }
+  this.updateTable();
 };
 
 function clickButton(i, j){
   var value = prato.getPlayGround()[i][j];
-  if(value == clear) prato.blankArea(i, j);
+  if(value == prato.elements.bomb){
+    prato.setEndGame();
+    prato.updateTable();
+  }
+  if(value == prato.elements.clear) prato.blankArea(i, j);
   else {
     prato.setPlayGroundVisual(value, i, j);
-    prato.createTable();
+    prato.updateTable();
   }
 
 }
@@ -124,11 +212,10 @@ function clickButton(i, j){
 
 
 function main(){
-  document.getElementById("prova").innerHTML = "swag";
-  prato = new Game(9);
+  prato = new Game(9, 9);
+  prato.ini();
   prato.createTable();
-  console.log(prato.getPlayGround());
-  //prato.blankArea(0,0);
+
 
 
 }
